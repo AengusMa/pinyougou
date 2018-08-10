ccff6c44951e544b460b551b7868a4a9ea0a82f8
mawenlong <15061886113@163.com>
2018-08-10 11:25:36 +0800

    添加注释

--- pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -1,77 +1,80 @@
 package com.pinyougou.manager.controller;
 
-import java.util.List;
-
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.common.pojo.PageResult;
 import com.common.pojo.Result;
+import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.sellergoods.service.BrandService;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alibaba.dubbo.config.annotation.Reference;
-import com.pinyougou.pojo.TbBrand;
-import com.pinyougou.sellergoods.service.BrandService;
+import java.util.List;
 
-
+/**
+ * @author mawenlong
+ * @date 2018/8/10
+ * 品牌管理控制器
+ */
 @RestController
 @RequestMapping("/brand")
 public class BrandController {
 
-	@Reference
-	private BrandService brandService;
-	
-	@RequestMapping("/findAll")
-	public List<TbBrand> findAll(){
-		return brandService.findAll();		
-	}
-	
-	@RequestMapping("/findPage")
-	public PageResult findPage(int page, int size){
-		return brandService.findPage(null,page, size);
-	}
-	
-	@RequestMapping("/add")
-	public Result add(@RequestBody TbBrand brand){
-		try {
-			brandService.add(brand);
-			return new Result(true, "增加成功");
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new Result(false, "增加失败");
-		}		
-	}
-	
-	@RequestMapping("/getById")
-	public TbBrand getById(Long id){
-		return brandService.getById(id);
-	}
-	
-	
-	@RequestMapping("/update")
-	public Result update(@RequestBody TbBrand brand){
-		try {
-			brandService.update(brand);
-			return new Result(true, "修改成功");
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new Result(false, "修改失败");
-		}		
-	}
-	
-	@RequestMapping("/delete")
-	public Result delete(Long [] ids){
-		try {
-			brandService.delete(ids);
-			return new Result(true, "删除成功1");
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new Result(false, "删除失败");
-		}	
-	}
-	
-	@RequestMapping("/search")
-	public PageResult search(@RequestBody TbBrand brand,int page,int size){
-		return brandService.findPage(brand, page, size);		
-	}
-	
+    @Reference
+    private BrandService brandService;
+
+    @RequestMapping("/findAll")
+    public List<TbBrand> findAll() {
+        return brandService.findAll();
+    }
+
+    @RequestMapping("/findPage")
+    public PageResult findPage(int page, int size) {
+        return brandService.findPage(null, page, size);
+    }
+
+    @RequestMapping("/add")
+    public Result add(@RequestBody TbBrand brand) {
+        try {
+            brandService.add(brand);
+            return new Result(true, "增加成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, "增加失败");
+        }
+    }
+
+    @RequestMapping("/getById")
+    public TbBrand getById(Long id) {
+        return brandService.getById(id);
+    }
+
+
+    @RequestMapping("/update")
+    public Result update(@RequestBody TbBrand brand) {
+        try {
+            brandService.update(brand);
+            return new Result(true, "修改成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, "修改失败");
+        }
+    }
+
+    @RequestMapping("/delete")
+    public Result delete(Long[] ids) {
+        try {
+            brandService.delete(ids);
+            return new Result(true, "删除成功1");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, "删除失败");
+        }
+    }
+
+    @RequestMapping("/search")
+    public PageResult search(@RequestBody TbBrand brand, int page, int size) {
+        return brandService.findPage(brand, page, size);
+    }
+
 }
