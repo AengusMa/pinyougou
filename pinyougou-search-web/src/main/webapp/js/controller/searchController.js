e4cc81f9eea2504860f0814fbf87aaf5ab09a93f
mawenlong <15061886113@163.com>
2018-08-24 23:54:49 +0800

    分页栏实现

--- pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -4,10 +4,12 @@ app.controller('searchController', function ($scope, searchService) {
                         "pageNo":1,"pageSize":40};
     //搜索
     $scope.search = function () {
+        $scope.searchMap.pageNo = parseInt($scope.searchMap.pageNo );
         searchService.search($scope.searchMap).success(
             function (response) {
                 $scope.resultMap = response;
-
+                //查询后显示第一页
+                $scope.searchMap.pageNo = 1;
                 buildPageLable();
 
             }
@@ -15,7 +17,27 @@ app.controller('searchController', function ($scope, searchService) {
     };
     buildPageLable=function(){
         $scope.pageLable=[];
-        for(var i=1;i<=$scope.resultMap.totalPages;i++){
+        var firstPage = 1;
+        var lastPage = $scope.resultMap.totalPages;
+        $scope.firstDot=true;
+        $scope.lastDot= true;
+
+        if($scope.resultMap.totalPages>5){
+            //当前页码小于3
+            if($scope.searchMap.pageNo<=3 ){
+                lastPage=5;
+                $scope.firstDot = false;
+            //显示后五页
+            }else if($scope.searchMap.pageNo>=$scope.resultMap.totalPages-2){
+                firstPage = $scope.resultMap.totalPages-4;
+                $scope.lastDot= false;
+            }else{
+                firstPage = $scope.searchMap.pageNo-2;
+                lastPage = $scope.searchMap.pageNo+2;
+            }
+        }
+
+        for(var i=firstPage;i<=lastPage;i++){
             $scope.pageLable.push(i)
         }
     };
@@ -36,5 +58,25 @@ app.controller('searchController', function ($scope, searchService) {
             delete $scope.searchMap.spec[key];
         }
     };
-
+    $scope.queryByPage = function (pageNo) {
+        if(pageNo<1 ||pageNo >$scope.resultMap.totalPages){
+            return;
+        }
+        $scope.searchMap.pageNo = pageNo;
+        $scope.search();
+    };
+    $scope.isTopPage =function () {
+        if($scope.searchMap.pageNo==1){
+            return true;
+        }else{
+            return false;
+        }
+    };
+    $scope.isEndPage =function () {
+        if($scope.searchMap.pageNo==$scope.resultMap.totalPages){
+            return true;
+        }else{
+            return false;
+        }
+    };
 });
