70485e02e258cbf3d1ad7e9a2fcbd7e5598d15ac
mawenlong <15061886113@163.com>
2018-08-28 23:58:29 +0800

    使用redis保存登录用户的购物车

--- pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/CartController.java
+++ pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/CartController.java
@@ -7,6 +7,7 @@ import com.common.utils.CookieUtil;
 import com.pinyougou.cart.service.CartService;
 import com.pinyougou.pojogroup.Cart;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/cart")
 public class CartController {
-    @Reference
+    @Reference(timeout=6000)
     private CartService cartService;
 
     @Autowired
@@ -39,12 +40,28 @@ public class CartController {
      */
     @RequestMapping("/findCartList")
     public List<Cart> findCartList() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
         String cartListString = CookieUtil.getCookieValue(request, "cartList", "UTF-8");
         if (cartListString == null || cartListString.equals("")) {
             cartListString = "[]";
         }
         List<Cart> cartList_cookie = JSON.parseArray(cartListString, Cart.class);
-        return cartList_cookie;
+        if ("anonymousUser".equals(username)) {
+            return cartList_cookie;
+        } else {
+            //已登录从redis中提取
+            List<Cart> cartList_redis = cartService.findCartListFromRedis(username);
+            //如果本地存在购物车
+            if(cartList_cookie.size()>0){
+                //合并购物车
+                cartList_redis=cartService.mergeCartList(cartList_redis, cartList_cookie);
+                //清除本地cookie的数据
+                CookieUtil.deleteCookie(request, response, "cartList");
+                //将合并后的数据存入redis
+                cartService.saveCartListToRedis(username, cartList_redis);
+            }
+            return cartList_redis;
+        }
     }
 
     /**
@@ -56,13 +73,25 @@ public class CartController {
      */
     @RequestMapping("/addGoodsToCartList")
     public Result addGoodsToCartList(Long itemId, Integer num) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        System.out.println("当前登录用户："+username);
         try {
             //获取购物车列表
             List<Cart> cartList =findCartList();
             cartList = cartService.addGoodsToCartList(cartList, itemId, num);
-            CookieUtil.setCookie(request, response, "cartList", JSON.toJSONString(cartList),3600*24,"UTF-8");
+            if("anonymousUser".equals(username)){
+                //如果是未登录，保存到cookie
+                CookieUtil.setCookie(request, response, "cartList",
+                        JSON.toJSONString(cartList),3600*24 ,"UTF-8");
+                System.out.println("向cookie存入数据");
+            }else{//如果是已登录，保存到redis
+                cartService.saveCartListToRedis(username, cartList);
+            }
             return new Result(true, "添加成功");
-        } catch (Exception e) {
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            return new Result(false, e.getMessage());
+        }catch (Exception e) {
             e.printStackTrace();
             return new Result(false, "添加失败");
         }
--- pinyougou-cart/pinyougou-cart-interface/src/main/java/com/pinyougou/cart/service/CartService.java
+++ pinyougou-cart/pinyougou-cart-interface/src/main/java/com/pinyougou/cart/service/CartService.java
@@ -19,4 +19,27 @@ public interface CartService {
      * @return java.util.List<com.pinyougou.pojogroup.Cart>
      */
     List<Cart> addGoodsToCartList(List<Cart> cartList, Long itemId, Integer num );
+    /**
+     * 将购物车保存到redis
+     *      
+     * @param username
+	 * @param cartList
+     * @return void
+     */
+     void saveCartListToRedis(String username,List<Cart> cartList);
+     /**
+      * 从redis中查询购物车
+      *      
+      * @param username
+      * @return java.util.List<com.pinyougou.pojogroup.Cart>
+      */
+     List<Cart> findCartListFromRedis(String username);
+    /**
+     * 合并购物车
+     *      
+     * @param cartList1
+	 * @param cartList2
+     * @return java.util.List<com.pinyougou.pojogroup.Cart>
+     */
+     List<Cart> mergeCartList(List<Cart> cartList1,List<Cart> cartList2);
 }
